df8d15886bbcfa7e30e356bdcffb12a74a2bf798
Vwing <vwing@uci.edu>
2016-04-26 23:13:05 -0700

    Got extendo legs working properly
    Still haven't gotten them to pick up mmoatia

---
+++ Assets/AnchorLeg.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnchorLeg : MonoBehaviour {
+    Vector3 initPos;
+	// Use this for initialization
+	void Start () {
+        initPos = transform.localPosition;
+        initPos.y += transform.localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = initPos;
+        newPos.y = initPos.y - transform.localScale.y;
+        transform.localPosition = newPos;
+	}
+}
---
+++ Assets/Sasabonsam.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sasabonsam : MonoBehaviour {
+    public Transform leftLeg;
+    public Transform rightLeg;
+    [Tooltip("Time in seconds to descend fully")]
+    public float descentTime = 3f;
+    [Tooltip("Time in seconds for legs to stay in tree")]
+    public float eatTime = 2f;
+    public State state = State.Descending;
+
+    private float maxScale = 4.3f; //max scale leg needs to be to reach mmoatia
+    private float minScale = .1f; //min scale leg needs to be to be hidden
+    private Vector3 maxScaleV;
+    private Vector3 minScaleV;
+    private float timer = 0f;
+    private float speed; //determined by descentTime and amount of scale needing to be covered
+
+    void Start()
+    {
+        maxScaleV = leftLeg.localScale;
+        maxScaleV.y = maxScale;
+        minScaleV = leftLeg.localScale;
+        minScaleV.y = minScale;
+        speed = (maxScale - minScale) / descentTime;
+    }
+
+    public enum State
+    {
+        Descending,
+        Waiting,
+        Ascending,
+        Eating
+    }
+
+	void UpdateState()
+    {
+        timer += Time.deltaTime;
+        switch (state)
+        {
+            case State.Eating:
+                if (timer >= eatTime)
+                {
+                    timer = 0f;
+                    state = State.Descending;
+                }
+                break;
+            case State.Descending:
+                if (timer >= descentTime)
+                {
+                    timer = 0f;
+                    leftLeg.localScale = maxScaleV;
+                    rightLeg.localScale = maxScaleV;
+                    state = State.Waiting;
+                }
+                break;
+            case State.Ascending:
+                if (timer >= descentTime)
+                {
+                    timer = 0f;
+                    leftLeg.localScale = minScaleV;
+                    rightLeg.localScale = minScaleV;
+                    state = State.Eating;
+                }
+                break;
+            case State.Waiting:
+                //temp to see if works
+                if (timer >= eatTime)
+                {
+                    timer = 0f;
+                    state = State.Ascending;
+                }
+                break;
+        }
+    }
+
+    Vector3 newScale;
+    void UpdateScale()
+    {
+        switch(state)
+        {
+            case State.Waiting:
+                break;
+            case State.Eating:
+                break;
+            case State.Ascending:
+                newScale = leftLeg.localScale;
+                newScale.y -= Time.deltaTime * speed;
+                leftLeg.localScale = newScale;
+                rightLeg.localScale = newScale;
+                break;
+            case State.Descending:
+                newScale = leftLeg.localScale;
+                newScale.y += Time.deltaTime * speed;
+                leftLeg.localScale = newScale;
+                rightLeg.localScale = newScale;
+                break;
+        }
+    }
+	
+	void Update ()
+    {
+        UpdateState();
+        UpdateScale();
+	}
+}
